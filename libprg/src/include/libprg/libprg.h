f6adcb7deb2ae82beed10a09ea1903bc30e7c06b
EvandroCon <evandrodbz@hotmail.com>
2026-03-26 18:15:09 -0300

    funções fila adicionada ao header

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -2,7 +2,7 @@
 #define LIBPRG_LIBPRG_H
 
 
-
+//PILHA
 typedef struct pilha pilha_t;
 pilha_t* criar_pilha(int capacidade);
 int empilhar(pilha_t* pilha, int* valor);
@@ -10,4 +10,11 @@ int desempilhar(pilha_t* pilha);
 int tamanho(pilha_t* pilha);
 bool vazia(pilha_t* pilha);
 int destruit_pilha(pilha_t* pilha);
+
+
+//FILA
+typedef struct fila fila_t;
+fila_t* criar_fila(int capacidade);
+void enfileirar(fila_t* fila, int valor);
+int destruir_fila(fila_t* fila);
 #endif
